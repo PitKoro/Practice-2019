0ada8c2f7765275e3192478fc378c99e8b9aaee9
Королев Пётр <37357614+lumberspin@users.noreply.github.com>
2019-06-15 17:33:44 +0300

    Delete table.php
    Для работоспособности скрипта он не нужен

--- table.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include('./conn.php');
-  $sql = mysqli_query($link, 'SELECT `id`, `name`, `description` FROM `Privileges`');
-  while ($result = mysqli_fetch_array($sql)) {
-    echo "{$result['name']}, {$result['description']}<br>";
-  }
-?>
